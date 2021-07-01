9f942a312c5c3d2e3811c96ef81757f2d4eb2c97
Tommy Chung <tommy.chung@quanta.corp-partner.google.com>
2021-07-01 09:47:56 +0800

    swap: add vector method

--- swap/swap.cpp
+++ swap/swap.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-//#include <string>
+#include <string>
+#include <vector>
 
 #define PI 3.1415926
 #define EXP 2.71828
@@ -20,6 +21,7 @@ int main()
 	double double1 = PI; double double2 = EXP;
 	std::string str1 = "Hello"; std::string str2 = "World";
 	const char *charptr1 = "Welcome"; const char *charptr2 = "to C++";
+	std::vector<char> vector_char1 = {'a', 'b', 'c'}; std::vector<char> vector_char2 = {'d', 'e'};
 
 	swap2<char>(char1, char2);
 	swap2<int>(int1, int2);
@@ -27,6 +29,7 @@ int main()
 	swap2<double>(double1, double2);
 	swap2<std::string>(str1, str2);
 	swap2<const char *>(charptr1, charptr2);
+	swap2<std::vector<char>>(vector_char1, vector_char2);
 
 	std::cout << "char swap result: char1 = " << char1 << ", char2 = " << char2 << std::endl;
 	std::cout << "int swap result: int1 = " << int1 << ", int2 = " << int2 << std::endl;
@@ -34,6 +37,10 @@ int main()
 	std::cout << "double swap result: double1 = " << double1 << ", double2 = " << double2 << std::endl;
 	std::cout << "string swap result: str1 = " << str1 << ", str2 = " << str2 << std::endl;
 	std::cout << "charptr swap result: charptr1 = " << charptr1 << ", charptr2 = " << charptr2 << std::endl;
+
+	std::string s1(vector_char1.begin(), vector_char1.end());
+	std::string s2(vector_char2.begin(), vector_char2.end());
+	std::cout << "vector_char swap result: vector_char1 = " << s1 << ", vector_char2 = " << s2 << std::endl;
 
 	return 0;
 }
